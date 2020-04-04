b79d7c04958e3506a1434857dc6fb2a79d87d786
irenekyc <gugu_irene@hotmail.com>
2020-04-04 12:22:00 -0500

    home and posts page filtering function

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+const filterContainer = document.querySelector('.filter-container')
+
+filterContainer.addEventListener('click', (e)=>{
+    const actionPoint =  e.target.id
+    if (actionPoint.includes('main')){
+        return document.querySelector(`.${actionPoint}`).style.display="block"
+    }
+    if(e.target.id.includes('filter')){
+       return console.log(e.target.id)
+    }
+    if (e.target.id.includes('sort')){
+        return console.log(e.target.id)}
+    
+    if (e.target.id === "search-confirm"){
+        return console.log(document.getElementById('searchKeyWords').value)
+    } 
+})
